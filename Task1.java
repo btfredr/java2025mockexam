bd213dd8efed3bbe598d503ebcdc4be2cd51001b
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-05-06 18:33:48 +0200

    Task 1 init

---
+++ Task1.java
@@ -0,0 +1,16 @@
+/*
+    Task 1 (10 points)
+    
+    Write a method that takes a double value as an argument and returns the smallest integer
+    number greater than or equal to the input that is divisible by 3.
+    Examples:
+    ● Input = 10.7 → Returns 12
+    ● Input = 9.0 → Returns 9
+    ● Input = -4.2 → Returns -3
+
+ */
+
+
+public class Task1 {
+
+}
