05e960ac9125e3a55971de04412c2c7054198ce0
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-05-06 18:40:46 +0200

    starting loop

--- Task2.java
+++ Task2.java
@@ -1,18 +1,32 @@
 /*
     Task 2 (15 points)
-    
+
     Write a Java program that uses nested for loops to print a diamond pattern of asterisks (*). The
     diamond’s maximum width is specified by the user as an odd number width. For example, if
     width = 5, the output should be:
+
     *
     ***
     *****
     ***
     *
+
     The program should prompt the user for width (assume it’s odd and positive) and print the
     diamond with proper spacing. Each row should be centered.
  */
 
+import java.util.Scanner;
+
 public class Task2 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the height: ");
+        int height = scanner.nextInt();
+
+        scanner.close();
+
+        for (int i = 1; i < height; i++) {
+            
+        }
+    }
 }
