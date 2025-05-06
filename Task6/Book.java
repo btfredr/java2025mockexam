6028cee770d7e3f27982e47ce256717ff7577abb
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-05-06 23:29:28 +0200

    added constructor

--- Task6/Book.java
+++ Task6/Book.java
@@ -1,5 +1,26 @@
 package Task6;
+/*
+    Task 6.5: Additional Fields in Book (1 point)
+    Add private fields to Book:
+    ● author (String)
+    ● isHardcover (boolean)
 
-public class Book {
-    
+    Task 6.6: Constructor for Book (3 points)
+    Add a constructor for Book that initializes all fields (superclass and subclass).
+
+    Task 6.7: Methods in Book (3 points)
+    Add methods getAuthor and isHardcover to return the respective field values.
+
+    Task 6.8: Override Print Method in Book (3 points)
+    Override the print method in Book to include all fields.
+ */
+public class Book extends Product {
+    private String author;
+    private boolean isHardcover;
+
+    public Book(int productID, String name, double price, String author, boolean isHardcover) {
+        super(productID, name, price);
+        this.author = author;
+        this.isHardcover = isHardcover;
+    }
 }
