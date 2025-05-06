abad96d8a74dbd21f3bd4970699d258a1ca7d926
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-05-06 22:40:53 +0200

    TASK 3 INIT

---
+++ Task3.java
@@ -0,0 +1,13 @@
+/*
+    Task 3 (15 points)
+    
+    Write a method that takes an integer n as an argument. If n is even, use a while loop to print
+    numbers from n down to n - 4 in descending order. If n is odd, print the cube of n (i.e., n * n * n).
+    Examples:
+    ● n = 8 → Prints: 8,7,6,5,
+ */
+
+
+public class Task3 {
+    
+}
