774f9daf1190d226ed5d32e84298e835ed96b3ba
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-05-06 22:48:24 +0200

    added logic

--- Task3.java
+++ Task3.java
@@ -1,6 +1,6 @@
 /*
     Task 3 (15 points)
-    
+
     Write a method that takes an integer n as an argument. If n is even, use a while loop to print
     numbers from n down to n - 4 in descending order. If n is odd, print the cube of n (i.e., n * n * n).
     Examples:
@@ -10,4 +10,23 @@
 
 public class Task3 {
     
+    public void evenOrCube(int n) {
+
+        int count = n;
+        int min = n - 4;
+        if (n % 2 == 0) {
+            while (count >= min) {
+                if (count > min) {
+                    System.out.print(count + ", ");
+                    count--;
+                } else {
+                    System.out.print(count);
+                }
+                System.out.println();
+            } 
+        } else {
+            System.out.println("Cube of n: ");
+            System.out.print((n*n)*2);
+        }
+    }
 }
