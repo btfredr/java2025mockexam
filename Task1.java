b62a6ae0f897620affeab05aecbc39cab9cf9317
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-05-06 18:37:26 +0200

    added method to task 1

--- Task1.java
+++ Task1.java
@@ -1,6 +1,6 @@
 /*
     Task 1 (10 points)
-    
+
     Write a method that takes a double value as an argument and returns the smallest integer
     number greater than or equal to the input that is divisible by 3.
     Examples:
@@ -12,5 +12,15 @@
 
 
 public class Task1 {
+    
+    public int greaterThanOrEqual (double n) {
+        int floor = (int) Math.floor(n);
 
+        if (floor % 3 == 0) {
+            int result = floor - 3;
+            return result;
+        } else {
+            return (int) floor;
+        }
+    }
 }
