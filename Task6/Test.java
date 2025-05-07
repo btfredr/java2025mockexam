342031f7be065d284c826ee34367cd8f2aa84d9f
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-05-07 12:08:37 +0200

    Test init

---
+++ Task6/Test.java
@@ -0,0 +1,12 @@
+package Task6;
+/*
+    Task 6.11: Test Class (5 points)
+    Create a Test class with a main method to create one Book and one Electronics instance and
+    call their print methods.
+
+    Note: Pay attention to access modifiers and OOP principles (inheritance, encapsulation,
+    polymorphism).
+ */
+public class Test {
+    
+}
