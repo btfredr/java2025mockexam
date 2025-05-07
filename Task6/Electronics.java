093122214425c7e7a12cd91ecbe3af5f0b9b3dfe
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-05-07 11:54:35 +0200

    added constructor

--- Task6/Electronics.java
+++ Task6/Electronics.java
@@ -1,5 +1,24 @@
 package Task6;
 
-public class Electronics {
+/*
+    Task 6.9: Additional Field in Electronics (2 points)
+    Add a private field warrantyYears (int) to Electronics.
+
+    Task 6.10: Polymorphic Print Methods in Electronics (4 points)
+    Add two methods in Electronics:
+    ● One to print only name and price.
+    ● One to print all fields, including warrantyYears.
+
+ */
+
+public class Electronics extends Product{
+    private int warrantyYears;
+
+    // Constructor
+    public Electronics (int productID, String name, double price, int warrantyYears) {
+        super(productID, name, price);
+        this.warrantyYears = warrantyYears;
+    }
+
     
 }
